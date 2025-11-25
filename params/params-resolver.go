1c738bb14bacdfb06f7ce4391384e67aa2f6e298
Artur Aronov <unionco@icloud.com>
2025-11-25 14:03:05 +0800

    add ParamsResover

---
+++ params/params-resolver.go
@@ -0,0 +1,25 @@
+package params
+
+import (
+	"log"
+	initcmd "market-data/src/init"
+	"os"
+)
+
+func ParamsResover() {
+	args := os.Args
+
+	if len(args) < 2 {
+		log.Fatalf("Expected subcommand.")
+	}
+
+	switch args[1] {
+	case "init":
+		runInitErr := initcmd.RunInit(args[2:])
+		if runInitErr != nil {
+			log.Fatalf("ParamsResover: init error %v\n%v\n", runInitErr, os.Stderr)
+		}
+	default:
+		log.Fatalf("Unknown subcommand: %v\n", args[1])
+	}
+}
