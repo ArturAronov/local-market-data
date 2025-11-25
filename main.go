66e91607b41e7a8a60ddb350e65f1643c2f93761
Artur Aronov <unionco@icloud.com>
2025-11-25 14:02:21 +0800

    maing.go: add ParamsResover

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"log"
-	company_ticker "market-data/src/company-tickers"
+	"market-data/params"
 
 	// user_agent "market-data/src/user-agent"
 	"os"
@@ -22,6 +21,8 @@ func main() {
 		log.Fatal(dbErr)
 	}
 
+	params.ParamsResover()
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
@@ -30,10 +31,10 @@ func main() {
 
 	defer db.Close()
 	// user_agent.InitDB(ctx, db)
-	_, _, err := company_ticker.GetCompanyTickers()
-	if err != nil {
-		fmt.Println(err)
-	}
+	// _, _, err := company_ticker.GetCompanyTickers()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
 	log.Println("Server starting on :7700")
 
