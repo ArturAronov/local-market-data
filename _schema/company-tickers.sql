a1855838257eb51f4dc735fa3ca4d0c2aed13675
Artur Aronov <unionco@icloud.com>
2026-01-11 14:46:23 +0800

    remove junk

--- _schema/company-tickers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS company_tickers (
-    cik     INTEGER PRIMARY KEY,
-    ticker  TEXT    NOT NULL UNIQUE,
-    title   TEXT    NOT NULL
-);
-
-
-CREATE INDEX IF NOT EXISTS idx_ticker ON company_tickers(ticker);
--- src/company-tickers/types.go
+++
@@ -1,9 +0,0 @@
-package company_ticker
-
-type SecEntry struct {
-	CIKStr int64  `json:"cik_str"`
-	Ticker string `json:"ticker"`
-	Title  string `json:"title"`
-}
-
-type SecResponse map[string]SecEntry
