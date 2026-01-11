27bc0c4addca56be40b0d9975a2ccf4bc18a0ca0
Artur Aronov <unionco@icloud.com>
2026-01-11 15:29:48 +0800

    add CompanyFactsC

---
+++ src/company-facts/controller.go
@@ -0,0 +1,25 @@
+package company_facts
+
+import (
+	"fmt"
+	"log"
+	"market-data/src/user"
+	"market-data/src/utils"
+)
+
+func GetCompanyFactsC(cik int) {
+	cikStr := fmt.Sprintf("%010d", cik)
+	url := fmt.Sprintf("https://data.sec.gov/submissions/CIK%s.json", cikStr)
+
+	email, emailErr := user.GetUserEmail()
+	if emailErr != nil {
+		log.Fatalf("[GetCompanyFactsC] Error getting user email: %v\n", emailErr)
+	}
+
+	body, bodyErr := utils.HttpReq(*email, url)
+	if bodyErr != nil {
+		log.Fatalf("[GetCompanyTickersC] failed to handle request %v", bodyErr)
+	}
+
+	fmt.Println(string(body))
+}
