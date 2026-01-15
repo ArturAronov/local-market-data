a23d1f375f56f7cf4810216076b438eb6a3af097
Artur Aronov <unionco@icloud.com>
2026-01-15 14:07:01 +0800

    params-resolver.go: add error return if no subcommand found

--- src/params/params-resolver.go
+++ src/params/params-resolver.go
@@ -8,23 +8,47 @@ import (
 	"os"
 )
 
+type Params string
+
+const (
+	TEST   Params = "test"
+	INIT   Params = "init"
+	UPDATE Params = "update"
+)
+
+var paramsMap = map[Params]string{
+	TEST:   string(TEST),
+	INIT:   string(INIT),
+	UPDATE: string(UPDATE),
+}
+
 func ParamsResover() {
 	args := os.Args
+
+	if len(args) == 1 {
+		log.Println("Subcommand is expected. Use any of subcommands below:")
+
+		for key := range paramsMap {
+			log.Printf("- %s", paramsMap[key])
+		}
+
+		os.Exit(1)
+	}
 
 	if len(args) < 2 {
 		log.Fatalf("Expected subcommand.")
 	}
 
 	switch args[1] {
-	case "test":
+	case paramsMap[TEST]:
 		// submission_data.GetSubmissionDataC(1018724)
 		company_info.GetCompanyFactsC(1018724)
-	case "init":
+	case paramsMap[INIT]:
 		runInitErr := initcmd.RunInit(args[2:])
 		if runInitErr != nil {
 			log.Fatalf("ParamsResover: init error %v\n%v\n", runInitErr, os.Stderr)
 		}
-	case "update":
+	case paramsMap[UPDATE]:
 		runInitErr := RunUpdate(args[2:])
 		if runInitErr != nil {
 			log.Fatalf("ParamsResover: init error %v\n%v\n", runInitErr, os.Stderr)
