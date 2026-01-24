725fc401cd401799ec63a92e4e0a733db98be3f9
Artur Aronov <unionco@icloud.com>
2026-01-25 00:37:33 +0800

    add InsertCompanyFactsR, InsertCompanyReportsR

--- src/company-info/repository.go
+++ src/company-info/repository.go
@@ -9,6 +9,8 @@ import (
 )
 
 const (
+	DELETE_COMPANY = `DELETE FROM company WHERE cik = ?;`
+	SELECT_ALL     = `SELECT cik, ticker, name FROM company;`
 	UPDATE_COMPANY = `
 		UPDATE company
 		SET
@@ -33,14 +35,35 @@ const (
 			latest_10q
 		FROM company
 		WHERE cik = ?;`
-	DELETE_COMPANY     = `DELETE FROM company WHERE cik = ?;`
-	SELECT_ALL         = `SELECT cik, ticker, name FROM company;`
 	INSERT_TICKER_INFO = `
 		INSERT OR IGNORE INTO company (
 			cik,
 			ticker,
 			name
 		) VALUES (?, ?, ?);`
+	INSERT_COMPANY_FACT = `
+		INSERT OR IGNORE INTO fact (
+			cik,
+			fact_key,
+			namespace,
+			label,
+			description,
+			unit
+		) VALUES (?, ?, ?, ?, ?, ?);`
+	INSERT_COMPANY_REPORT = `
+		INSERT OR IGNORE INTO report_data (
+			cik,
+			fact_key,
+			start,
+			end,
+			val,
+			accn,
+			fy,
+			fp,
+			form,
+			filed,
+			frame
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
 )
 
 type Repository struct {
@@ -51,7 +74,7 @@ func NewRepository(db *sql.DB) *Repository {
 	return &Repository{db: db}
 }
 
-func (r *Repository) GetCompanyR(cik int) (*Company, error) {
+func (r *Repository) GetCompanyR(cik int) (*DbCompany, error) {
 	rows, rowsErr := r.db.Query(SELECT_COMPANY, cik)
 	if rowsErr != nil {
 		return nil, fmt.Errorf(
@@ -63,7 +86,7 @@ func (r *Repository) GetCompanyR(cik int) (*Company, error) {
 
 	defer rows.Close()
 
-	var response Company
+	var response DbCompany
 	rowErr := rows.Scan(
 		&response.Cik,
 		&response.Name,
@@ -88,7 +111,7 @@ func (r *Repository) GetCompanyR(cik int) (*Company, error) {
 	return &response, nil
 }
 
-func (r *Repository) UpdateCompanyR(company Company) error {
+func (r *Repository) UpdateCompanyR(company DbCompany) error {
 	result, resultErr := r.db.Exec(
 		UPDATE_COMPANY,
 		company.Sic,
@@ -125,8 +148,134 @@ func (r *Repository) UpdateCompanyR(company Company) error {
 	return nil
 }
 
+func (r *Repository) InsertCompanyFactsR(data []DbFact) error {
+	if len(data) == 0 {
+		log.Println("[InsertCompanyFactsR] No facts to insert")
+		return nil
+	}
+
+	log.Println("[InsertCompanyFactsR] Inserting company facts into db")
+
+	tx, err := r.db.Begin()
+	if err != nil {
+		return fmt.Errorf("[InsertCompanyFactsR] Failed to start transaction: %w", err)
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p)
+		}
+	}()
+
+	stmt, err := tx.Prepare(INSERT_COMPANY_FACT)
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("[InsertCompanyFactsR] Failed to prepare statement: %w", err)
+	}
+	defer stmt.Close()
+
+	var totalRowsAffected int64
+
+	for _, entry := range data {
+		res, err := stmt.Exec(
+			entry.Cik,
+			entry.FactKey,
+			entry.Namespace,
+			entry.Label,
+			entry.Description,
+			entry.Unit,
+		)
+		if err != nil {
+			tx.Rollback()
+			return fmt.Errorf("[InsertCompanyFactsR] Failed to insert data: %w", err)
+		}
+
+		if rowsAffected, err := res.RowsAffected(); err == nil {
+			totalRowsAffected += rowsAffected
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("[InsertCompanyFactsR] Failed to commit: %w", err)
+	}
+
+	log.Printf(
+		"[InsertCompanyFactsR] Inserted %d facts (%d new rows)",
+		len(data), totalRowsAffected,
+	)
+
+	return nil
+}
+
+func (r *Repository) InsertCompanyReportsR(data []DbReport) error {
+	if len(data) == 0 {
+		log.Println("[InsertCompanyReportsR] No facts to insert")
+		return nil
+	}
+
+	log.Println("[InsertCompanyReportsR] Inserting company facts into db")
+
+	tx, err := r.db.Begin()
+	if err != nil {
+		return fmt.Errorf("[InsertCompanyReportsR] Failed to start transaction: %w", err)
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p)
+		}
+	}()
+
+	stmt, err := tx.Prepare(INSERT_COMPANY_REPORT)
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("[InsertCompanyReportsR] Failed to prepare statement: %w", err)
+	}
+	defer stmt.Close()
+
+	var totalRowsAffected int64
+
+	for _, entry := range data {
+		res, err := stmt.Exec(
+			entry.Cik,
+			entry.FactKey,
+			entry.Start,
+			entry.End,
+			entry.Val,
+			entry.Accn,
+			entry.Fy,
+			entry.Fp,
+			entry.Form,
+			entry.Filed,
+			entry.Frame,
+		)
+		if err != nil {
+			tx.Rollback()
+			return fmt.Errorf("[InsertCompanyReportsR] Failed to insert data: %w", err)
+		}
+
+		if rowsAffected, err := res.RowsAffected(); err == nil {
+			totalRowsAffected += rowsAffected
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("[InsertCompanyReportsR] Failed to commit: %w", err)
+	}
+
+	log.Printf(
+		"[InsertCompanyReportsR] Inserted %d reports (%d new rows)",
+		len(data), totalRowsAffected,
+	)
+
+	return nil
+}
+
 func (r *Repository) InsertTickerInfoR(data *SecEntryRes) error {
 	log.Println("[InsertTickerInfoR] Inserting company ticker info into db")
+
 	tx, txErr := r.db.Begin()
 	if txErr != nil {
 		return fmt.Errorf("[InsertTickerInfoR] Failed to start transaction: %w", txErr)
@@ -147,7 +296,10 @@ func (r *Repository) InsertTickerInfoR(data *SecEntryRes) error {
 
 	query, queryErr := tx.Prepare(INSERT_TICKER_INFO)
 	if queryErr != nil {
-		return fmt.Errorf("[InsertTickerInfoR] Failed to prepare insert statement: %w", queryErr)
+		return fmt.Errorf(
+			"[InsertTickerInfoR] Failed to prepare insert statement: %w",
+			queryErr,
+		)
 	}
 
 	defer query.Close()
@@ -168,7 +320,10 @@ func (r *Repository) InsertTickerInfoR(data *SecEntryRes) error {
 		}
 	}
 
-	log.Printf("[InsertTickerInfoR] Completed inserting company ticker info, rows affected: %d", totalRowsAffected)
+	log.Printf(
+		"[InsertTickerInfoR] Completed inserting company ticker info, rows affected: %d",
+		totalRowsAffected,
+	)
 
 	return nil
 }
