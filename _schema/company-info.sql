d2fcafb0a9daa72e64773c21e03603b396421512
Artur Aronov <unionco@icloud.com>
2026-01-25 00:35:08 +0800

    company-info wip

--- _schema/company-info.sql
+++ _schema/company-info.sql
@@ -14,31 +14,33 @@ CREATE TABLE IF NOT EXISTS company (
 );
 
 CREATE TABLE IF NOT EXISTS fact (
-    id          SERIAL      PRIMARY KEY,
-    fact_key    TEXT        NOT NULL,
+    fact_key    TEXT        PRIMARY KEY,
+    cik         INTEGER     NOT NULL,
     namespace   TEXT        NOT NULL,
     label       TEXT,
     description TEXT,
     unit        TEXT,
 
-    UNIQUE(fact_key, namespace),
+    UNIQUE(cik, fact_key, namespace),
+    FOREIGN KEY (cik) REFERENCES company(cik),
     CHECK (namespace IN ('dei', 'ifrs-full', 'invest', 'srt', 'us-gaap', 'custom'))
 );
 
 CREATE TABLE IF NOT EXISTS report_data (
-  id        INTEGER         PRIMARY KEY,
-  cik       INTEGER         NOT NULL,
-  fact_id   INTEGER         NOT NULL,
-  start     TIMESTAMP,
-  end       TIMESTAMP,
-  val       DECIMAL(20,4)   NOT NULL,
-  accn      TEXT,
-  fy        SMALLINT,
-  fp        TEXT,
-  form      TEXT            NOT NULL,
-  filed     TIMESTAMP       NOT NULL,
-  frame     TEXT,
+    id        INTEGER         PRIMARY KEY,
+    cik       INTEGER         NOT NULL,
+    fact_key  TEXT            NOT NULL,
+    start     TIMESTAMP,
+    end       TIMESTAMP,
+    val       DECIMAL(20,4)   NOT NULL,
+    accn      TEXT            NOT NULL,
+    fy        SMALLINT,
+    fp        TEXT,
+    form      TEXT            NOT NULL,
+    filed     TIMESTAMP       NOT NULL,
+    frame     TEXT,
 
-  FOREIGN KEY (cik) REFERENCES company(cik),
-  FOREIGN KEY (fact_id) REFERENCES fact(id)
+    UNIQUE(cik, fact_key, start, end, accn, form),
+    FOREIGN KEY (cik) REFERENCES company(cik),
+    FOREIGN KEY (fact_key) REFERENCES fact(fact_key)
 );
